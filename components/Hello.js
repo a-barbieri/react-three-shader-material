86d5e60404616af9886e2cb0f913add00c571b7f
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-23 12:46:23 +0200

    .gitignore Fix

---
+++ components/Hello.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react'
+
+export class Hello extends Component {
+   render() {
+      return (<p>Hello NextJS</p>)
+   }
+}
---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import {Hello} from "../components/Hello";
+
+function Home() {
+   return <Hello />
+}
+
+export default Home;
