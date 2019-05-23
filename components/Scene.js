73918aec4f4aab59bd486b6a08e32541c6dd1c7e
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-23 17:33:11 +0200

    Add background shaders

--- components/Scene.js
+++ components/Scene.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react'
 import * as THREE from 'three'
 import {Clock} from './Clock'
+import {vertex} from './shaders/background.vert.js'
+import {fragment} from './shaders/background.frag.js'
 
 export class Scene extends Component {
    constructor(state) {
@@ -13,9 +15,10 @@ export class Scene extends Component {
       this.camera = new THREE.OrthographicCamera(-window.innerWidth, window.innerWidth, -window.innerHeight, window.innerHeight, 1, 2000)
       this.camera.position.y = 400;
 
-      this.renderer = new THREE.WebGLRenderer();
+      this.renderer = new THREE.WebGLRenderer({ alpha: true });
       this.renderer.setSize(window.innerWidth, window.innerHeight);
       this.renderer.setPixelRatio(1);
+      this.renderer.setClearColor( 0x000000, 0 );
       this.renderer.domElement.id = "scene_test";
 
       this.initialize();
@@ -25,34 +28,79 @@ export class Scene extends Component {
       }
    }
 
+   /**
+    *
+    */
    initialize() {
 
+      // Create Clock
+      this.clock = new Clock();
+
+      // Set Lights
       let ambientLight = new THREE.AmbientLight( 0xcccccc, 0.4 );
       this.scene.add( ambientLight );
 
+      // Create Background Plane
+      this.createBackground();
+
+      // Create Sphere
       let material = new THREE.MeshNormalMaterial();
       this.sphere = new THREE.Mesh( new THREE.SphereBufferGeometry( 75, 20, 10 ), material );
-      //object.position.set( - 300, 0, 200 );
       this.scene.add( this.sphere );
 
-      this.clock = new Clock();
-
+      // Launch animation process
       this.animate();
    }
 
+   /**
+    *
+    */
    animate() {
 
+      // Update sphere position
       let angle = this.clock.elapsedTime() * 0.001;
       this.sphere.position.x = 500 * Math.sin(angle);
       this.sphere.position.z = 500 * Math.cos(angle);
 
+      // Bind function to next frame
       requestAnimationFrame( this.animate );
+      // Render ThreeJs Scene
       this.renderScene();
    }
 
+   /**
+    *
+    */
    renderScene() {
       this.camera.lookAt( this.scene.position );
       this.renderer.render( this.scene, this.camera );
+   }
+
+   /**
+    *
+    */
+   createBackground() {
+      // create a background
+      const backgroundPlane = new THREE.PlaneBufferGeometry(
+         window.innerWidth,
+         window.innerHeight
+      );
+
+      let backgroundMaterial = new THREE.ShaderMaterial({
+         uniforms: {
+            colorB: {type: 'vec3', value: new THREE.Color(0xACB6E5)},
+            colorA: {type: 'vec3', value: new THREE.Color(0x74ebd5)}
+         },
+         vertexShader: vertex,
+         fragmentShader: fragment
+      });
+
+      // Create mesh
+      const mesh = new THREE.Mesh(backgroundPlane, backgroundMaterial);
+      mesh.name = "bg";
+      this.scene.add(mesh);
+
+      window.mesh = mesh;
    }
 
    render() {
---
+++ components/shaders/background.frag.js
@@ -0,0 +1,8 @@
+export const fragment = `
+uniform vec3 colorA;
+uniform vec3 colorB;
+varying vec3 vUv;
+
+void main() {
+    gl_FragColor = vec4(mix(colorA, colorB, vUv.z), 1.0);
+}`
