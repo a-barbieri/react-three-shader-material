c163f0c561cd7c217849f2630c9c05ca1b54629a
Lorenzo Rivosecchi <lorenzo.rivosecchi@libero.it>
2019-05-24 16:12:34 +0200

    Add sample image

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import {Scene} from "../components/Scene";
 
+import apple from '../assets/images/apple.jpeg'
+
 function Home() {
    return (
       <div>
+         <img src={apple} />
          <Scene />
       </div>
    )
